1da6c782bb60c6508fe057cfcf7acb0784771e4c
Davide Arcuri <davide.arcuri@leonardo.com>
2023-12-12 09:44:20 +0100

    day 12

---
+++ 2023/012/main_2.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+
+
+@lru_cache
+def gen_arr(text, options):
+    if not text:
+        return 0 if options else 1
+    if text.startswith("."):
+        return gen_arr(text.strip("."), options)
+    if text.startswith("?"):
+        return gen_arr(text.replace("?", ".", 1), options) + gen_arr(
+            text.replace("?", "#", 1), options
+        )
+    if text.startswith("#"):
+        if not options:
+            return 0
+        if len(text) < options[0]:
+            return 0
+        if any(c == "." for c in text[: options[0]]):
+            return 0
+        if len(options) <= 1:
+            return gen_arr(text[options[0] :], options[1:])
+        if len(text) < options[0] + 1 or text[options[0]] == "#":
+            return 0
+        return gen_arr(text[options[0] + 1 :], options[1:])
+    raise Exception("no other branches possible")
+
+
+with open("input.txt") as input_file:
+    input_lines = input_file.readlines()
+    input_lines = [line.strip("\n") for line in input_lines]
+    totals = 0
+    for line in input_lines:
+        text, options = line.split(" ")
+        options = tuple(int(x) for x in options.split(","))
+        totals += gen_arr("?".join([text] * 5), options * 5)
+    print(totals)
